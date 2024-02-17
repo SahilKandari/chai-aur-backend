a565301bac8531380ed763d74c1f517bb83d2f7a
Sahil Kandari <kandarisahil13@gmail.com>
2024-02-18 00:03:41 +0530

    Added Utils Files ApiError, ApiResponse, asyncHandler

--- src/index.js
+++ src/index.js
@@ -6,7 +6,19 @@ dotenv.config({
   path:'./env'
 });
 
-connectDB();
+connectDB()
+.then(() => {
+  app.on('error', (error) =>{
+    console.log(error,'error');
+    throw(error);
+  });
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at ${process.env.PORT}`);
+  });
+})
+.catch((err) => {
+  console.log('MongoDB Connection Failed!', err);
+});
 
 /*
 import mongoose from "mongoose";
